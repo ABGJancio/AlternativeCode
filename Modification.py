fe151e23d57a14704f8e6668a296848332aeb0c8
ABGJancio <77855688+ABGJancio@users.noreply.github.com>
2021-01-22 22:39:31 +0100

    Better shopping

--- Modification.py
+++ Modification.py
@@ -1,23 +1,8 @@
 shopping = {
-    'piekarnia': ['chleb', 'pączek', 'bułki'],
-    'warzywniak': ['marchew', 'seler', 'rukola']
-}
-q = []
-print('\nLista zakupów')
-for shop, prod_list in shopping.items():
-    shop = shop.capitalize()
-    prod_list = [prod.capitalize() for prod in prod_list]
-    q.append(len(prod_list))
-    print('Idę do', shop, end = ', ')
-    print('kupuję tu następujące rzeczy:', prod_list, end = '.\n')
-print('W sumie kupuję', sum(q), 'produktów.\n')
-
-# SecondWay
-
-shopping = {
     'piekarnia': ['chleb', 'pączek', 'bułki', 'ciastko'],
     'warzywniak': ['marchew', 'seler', 'rukola'],
-    'drogeria': ['szampon', 'krem do rąk']
+    'drogeria': ['szampon', 'krem do rąk'],
+    'monopolowy': ['whisky', 'cognac', 'champagne']
 }
 
 for shopname in shopping.keys():
@@ -28,9 +13,8 @@ for shop, prod_list in shopping.items():
     new_prod_list = [prod.capitalize() for i, prod in enumerate(prod_list)]
     shopping[shop] = new_prod_list
 
-q = []
-
 print('\nLista zakupów')
+q = []
 for shop, prod_list in shopping.items():
     print('Idę do', shop, end = ', ')
     print('kupuję tu następujące rzeczy:', prod_list, end = '.\n')
