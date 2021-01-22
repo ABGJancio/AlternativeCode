8c5aed2dc38f4bfc6e9a8b6202905e2b7c832b6d
ABGJancio <77855688+ABGJancio@users.noreply.github.com>
2021-01-22 22:45:29 +0100

    Final task output

--- shopping_list.py
+++ shopping_list.py
@@ -11,29 +11,3 @@ for shop, prod_list in shopping.items():
     print('Idę do', shop, end = ', ')
     print('kupuję tu następujące rzeczy:', prod_list, end = '.\n')
 print('W sumie kupuję', sum(q), 'produktów.\n')
-
-# SecondWay
-
-shopping = {
-    'piekarnia': ['chleb', 'pączek', 'bułki', 'ciastko'],
-    'warzywniak': ['marchew', 'seler', 'rukola'],
-    'drogeria': ['szampon', 'krem do rąk'],
-    'monopolowy': ['whisky', 'cognac', 'champagne']
-}
-
-for shopname in shopping.keys():
-    new_shopname = shopname.capitalize()
-    shopping[new_shopname] = shopping.pop(shopname)
-
-for shop, prod_list in shopping.items():
-    new_prod_list = [prod.capitalize() for prod in prod_list]
-    shopping[shop] = new_prod_list
-
-q = []
-
-print('\nLista zakupów')
-for shop, prod_list in shopping.items():
-    print('Idę do', shop, end = ', ')
-    print('kupuję tu następujące rzeczy:', prod_list, end = '.\n')
-    q.append(len(prod_list))
-print('W sumie kupuję', sum(q), 'produktów.\n')
